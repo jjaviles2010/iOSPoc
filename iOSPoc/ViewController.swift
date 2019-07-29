0d2344068d14f7c05bc57c905ef47c452337728a
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-07-29 00:03:23 -0300

    Including label and button

--- iOSPoc/ViewController.swift
+++ iOSPoc/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setDefaultText()
     }
 
 
+    func setDefaultText() {
+        myLabel.text = "Hello!"
+        myTextField.text = "Type your name"
+        myButton.setTitle("Greet!", for: .normal)
+    }
+    
+    
+    @IBAction func clickMyButton(_ sender: Any) {
+        if let text = myTextField.text {
+            myLabel.text = "Hello! \(text)"
+        }
+    }
+    
 }
 

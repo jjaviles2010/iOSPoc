3e5edf8efb6696af2cfcf4f53a4800e70004bf5c
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-07-28 23:33:11 -0300

    Initial Commit

---
+++ iOSPoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPoc
+//
+//  Created by Jose Javier on 28/07/19.
+//  Copyright © 2019 Jose Javier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
